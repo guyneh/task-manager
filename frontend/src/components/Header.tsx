0c00f948c2b5a724075e835d01af9be988021da7
guyneh <guynehushtan@gmail.com>
2024-07-05 10:48:19 +0100

    vertical alignment fixed when user is logged in

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 // Header component with Task Manager title and Sign In button
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Auth from './auth/AuthModal';
 import { useAuth } from '../context/AuthContext';
 import ProfileModal from './auth/ProfileModal';
@@ -22,6 +22,10 @@ const Header: React.FC = () => {
     const [showAuth, setShowAuth] = useState(false);
     const [showProfile, setShowProfile] = useState(false);
 
+    useEffect(() => {
+        console.log(authState)
+    })
+
     return (
         <div className="header-container">
             <div className="date-container">
@@ -29,13 +33,16 @@ const Header: React.FC = () => {
             </div>
             <h1 className="header-title">Task Manager</h1>
             {authState.user ? (
-                <div className='profile-picture-container'>
-                    <div onClick={() => setShowProfile(true)}>
+                <div className="profile-container" onClick={() => setShowProfile(true)}>
+                    <div className="profile-picture-container">
                         <ProfilePicture avatarUrl={authState?.user?.avatar || "avatar.png"} />
                     </div>
+                    <span>{authState?.user?.name}</span>
                 </div>
             ) : (
-                <button className="sign-in-button" onClick={() => setShowAuth(true)}>Sign In</button>
+                <div className="auth-container">
+                    <button className="sign-in-button" onClick={() => setShowAuth(true)}>Sign In</button>
+                </div>
             )}
             {showAuth && <Auth onClose={() => setShowAuth(false)} />}
             {showProfile && <ProfileModal onClose={() => setShowProfile(false)} />}
